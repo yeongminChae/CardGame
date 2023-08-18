52b90dafca392cc7eba00812da7049e8a698f03f
Yeongmin_Chae <codudals98@naver.com>
2023-08-18 20:15:27 +0900

    Initial Commit

---
+++ Memorize_Assingment/Memorize_AssingmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_AssingmentApp.swift
+//  Memorize_Assingment
+//
+//  Created by 채영민 on 2023/08/18.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_AssingmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
